c8a7d7f920b54939bdc0fb921a76dca22bd5568c
mohaaliothman <othman.othman2004v@gmail.com>
2026-06-05 20:10:33 +0300

    just add

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,14 @@
 import sys 
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name =exc_tb.tb_frame.f_code.co_filename
     error_massage = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-    file_name, exc_tb.tb_lineno, str(error)
+    file_name, exc_tb.tb_lineno, str(error))
 
     return error_massage
-    )
+    
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
@@ -16,3 +17,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by zero error occurred")
+        raise CustomException(e, sys) 
